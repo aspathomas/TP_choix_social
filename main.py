36305f61cc37884c4137d19f89f53773ce6524ee
thomas_aspa <aspa.thomas@gmail.com>
2024-03-11 09:10:02 +0100

    preference

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 pays = ["Serbia", "Bulgaria", "Macedonia", "Romania", "Greece", "Montenegro","Albania", "Bosnia and Herzegovina", "Croatia", "Slovenia"]
 critere = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20"]
-donnees = pd.read_csv("donnees.csv", header=None)
-poids = pd.read_csv("poids.csv", header=None)
+donnees = pd.read_csv("data/donnees.csv", header=None)
+poids = pd.read_csv("data/poids.csv", header=None)
 
 # Changement de poids pour que le total soit égale à 1
 poids.iloc[0, 4] = 0.08
@@ -47,33 +47,56 @@ for i_first in range(len(pays)):
         matrice_electre_preference[i_first][i_second] = score_electre_preference
 
 matrice = pd.DataFrame(matrice_promethee, index=pays, columns=pays)
-matrice.to_csv("matrice_promethee.csv", sep=';', decimal=',', float_format='%.2f')
+matrice.to_csv("generatedData/matrice_promethee.csv", sep=';', decimal=',', float_format='%.2f')
 matrice = pd.DataFrame(matrice_electre, index=pays, columns=pays)
-matrice.to_csv("matrice_electre.csv", sep=';', decimal=',', float_format='%.2f')
+matrice.to_csv("generatedData/matrice_electre.csv", sep=';', decimal=',', float_format='%.2f')
 matrice = pd.DataFrame(matrice_promethee_preference, index=pays, columns=pays)
-matrice.to_csv("matrice_promethee_preference.csv", sep=';', decimal=',', float_format='%.2f')
+matrice.to_csv("generatedData/matrice_promethee_preference.csv", sep=';', decimal=',', float_format='%.2f')
 matrice = pd.DataFrame(matrice_electre_preference, index=pays, columns=pays)
-matrice.to_csv("matrice_electre_preference.csv", sep=';', decimal=',', float_format='%.2f')
+matrice.to_csv("generatedData/matrice_electre_preference.csv", sep=';', decimal=',', float_format='%.2f')
 
 #Calcul des flux
 # Calcul flux positifs Φ+
 fplus_data = matrice_promethee.sum(axis=1)
 fplus = pd.DataFrame({"Country": pays, "fplus": fplus_data})
 fplus = fplus.sort_values(by='fplus', ascending=False)
-fplus.to_csv("fplus.csv", sep=';', decimal=',', float_format='%.2f')
+fplus.to_csv("generatedData/fplus.csv", sep=';', decimal=',', float_format='%.2f')
 
 # Calcul flux négatifs Φ-
 fmoins_data = matrice_promethee.sum(axis=0)
 fmoins = pd.DataFrame({"Country": pays, "fmoins": fmoins_data})
 fmoins = fmoins.sort_values(by='fmoins', ascending=True)
-fmoins.to_csv("fmoins.csv", sep=';', decimal=',', float_format='%.2f')
+fmoins.to_csv("generatedData/fmoins.csv", sep=';', decimal=',', float_format='%.2f')
 
 # Calcul flux net
 fnet_data = fplus_data - fmoins_data
 fnet = pd.DataFrame({"Country": pays, "fnet": fnet_data})
 fnet = fnet.sort_values(by='fnet', ascending=False)
-fnet.to_csv("fnet.csv", sep=';', decimal=',', float_format='%.2f')
+fnet.to_csv("generatedData/fnet.csv", sep=';', decimal=',', float_format='%.2f')
+
+
+#Calcul des flux preference
+# Calcul flux positifs preference Φ+
+fplus_data_preference = matrice_promethee_preference.sum(axis=1)
+fplus_preference = pd.DataFrame({"Country": pays, "fplus": fplus_data_preference})
+fplus_preference = fplus_preference.sort_values(by='fplus', ascending=False)
+fplus_preference.to_csv("generatedData/fplus_preference.csv", sep=';', decimal=',', float_format='%.2f')
+
+# Calcul flux négatifs preference Φ-
+fmoins_data_preference = matrice_promethee.sum(axis=0)
+fmoins_preference = pd.DataFrame({"Country": pays, "fmoins": fmoins_data_preference})
+fmoins_preference = fmoins_preference.sort_values(by='fmoins', ascending=True)
+fmoins_preference.to_csv("generatedData/fmoins_preference.csv", sep=';', decimal=',', float_format='%.2f')
+
+# Calcul flux net preference
+fnet_data_preference = fplus_data_preference - fmoins_data_preference
+fnet_preference = pd.DataFrame({"Country": pays, "fnet": fnet_data_preference})
+fnet_preference = fnet_preference.sort_values(by='fnet', ascending=False)
+fnet_preference.to_csv("generatedData/fnet_preference.csv", sep=';', decimal=',', float_format='%.2f')
 
 print(fplus)
 print(fmoins)
 print(fnet)
+print(fplus_preference)
+print(fmoins_preference)
+print(fnet_preference)
